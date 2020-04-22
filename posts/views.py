2edade3e0aa77fdaa4d21ee697055c27739bfd9c
Contrigra <gaponov60b@gmail.com>
2020-04-22 15:43:15 +0300

    commit -m "did some linting"

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-
-
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:10]
     return render(request, "index.html", {"posts": latest})
