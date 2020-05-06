bea5b4b5c8f23ed0b544c793e27366583714ce08
Contrigra <gaponov60b@gmail.com>
2020-05-06 14:05:57 +0300

    Learned wrappers, connected emails, decorators, added user, registration and templates for them

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-
+from django import forms
 
 User = get_user_model()
+
 
 # Creating our own class for signing up, built upon built-in UserCreationForm
 class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("first_name", "last_name", "username", "email")
+
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
